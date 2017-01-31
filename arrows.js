2a3edc9e100853f5dfaf4b605921c2fbb0c094ee
Dimitri Couwenberg <dimitri.couwenberg@team.telstra.com>
2017-01-31 17:11:15 +1100

    update

--- arrows.js
+++ arrows.js
@@ -1,11 +1,11 @@
-function b(x) {
-    x => x * 2
-}
-
+var b = (x) => {return x * 2}
 
 
 //https://www.sitepoint.com/es6-arrow-functions-new-fat-concise-syntax-javascript/
 
-var multiply = (x, y) => { return x * y };
+var multiply = (x, y) => {
+    return x * y
+};
 
-console.log('testing ',multiply(4,2))
+console.log('testing=b(4)', b(4))
+console.log('testing ', multiply(4, 2))
