9f626ffca4ed44f22634d1c94ee0597c550133bc
Dimitri Couwenberg <couwenberg@gmail.com>
2017-01-31 17:07:19 +1100

    ref

--- arrows.js
+++ arrows.js
@@ -3,6 +3,9 @@ function b(x) {
 }
 
 
+
+//https://www.sitepoint.com/es6-arrow-functions-new-fat-concise-syntax-javascript/
+
 var multiply = (x, y) => { return x * y };
 
 console.log('testing ',multiply(4,2))
