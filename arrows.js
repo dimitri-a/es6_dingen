82f1229278e3a39bea4119bd6fd4a8d59056b55e
Dimitri Couwenberg <dimitri.couwenberg@team.telstra.com>
2017-02-01 15:20:40 +1100

    more arrow

--- arrows.js
+++ arrows.js
@@ -7,5 +7,22 @@ var multiply = (x, y) => {
     return x * y
 };
 
+
+//var f = ([a, b] = [1, 2], {x: c} = {x: a + b}) => a + b + c;
+
+//console.log('testing=f()', f())
+
 console.log('testing=b(4)', b(4))
 console.log('testing ', multiply(4, 2))
+
+
+var materials = [
+    "Hydrogen",
+    "Helium",
+    "Lithium",
+    "Beryllium"
+];
+
+var le = materials.filter(mat => mat.length > 7);
+console.log('testing=le', le)
+
